9213da8dcf81a799e26a46cc64ec95daf859ce5e
vaishnavme <vaishnavchandurkar21@outlook.com>
2020-11-29 12:44:56 +0530

    Full featured app Done.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// inital requirement
+// get input
+const inputText = document.querySelector("#text-input"); 
+// show output
+const outputPlace = document.querySelector("#text-output");
+// btn to convert
+const translateBtn = document.querySelector("#btn-translate");
+
+translateBtn.addEventListener("click", translateButtonHandler);
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function translateButtonHandler(event) {
+    inputInfo = inputText.value;
+    var urlCall = buildURL(inputInfo);
+    console.log(urlCall);
+    fetch(urlCall)
+        .then(respose => respose.json())
+        .then(json => {
+            outputPlace.innerText = json.contents.translated;
+        })
+}
+
+function buildURL(inputData) {
+    return `${url}?text=${inputData}`;
+}
